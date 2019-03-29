94f3c93ad8264a57da4eb91024326e0001d22b9b
sonalibhagwat <sonali.m.bhagwat@gmail.com>
2019-03-29 11:25:19 +0530

    changes in con file

--- config/connection.php
+++ config/connection.php
@@ -2,6 +2,6 @@
 $servername = 'localhost';
 $username   = 'root';
 $password   = '';
-$dbname     = 'Foodcor';
+$dbname     = 'Foodcor1';
 $con = new mysqli($servername,$username,$password,$dbname) or die(mysqli_error($con));
 ?>
