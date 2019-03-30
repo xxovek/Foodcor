5db5d5c9eaf556c96c40dc94c41127fc0089ac98
sonalibhagwat <sonali.m.bhagwat@gmail.com>
2019-03-30 15:29:24 +0530

    additemDetails and additemUserside updated

--- src/addItemUserSide.php
+++ src/addItemUserSide.php
@@ -4,8 +4,7 @@ session_start();
 $companyId = $_SESSION['company_id'];
 $response = [];
 $method = $_SERVER['REQUEST_METHOD'];
-if($method == "PUT")
-{
+if($method == "PUT"){
   parse_str(file_get_contents("php://input"), $_PUT);
 //   $ItemId              = $_PUT['ItemId'];
   $ItemName     = mysqli_real_escape_string($con,$_PUT['ItemName']);
@@ -16,25 +15,30 @@ if($method == "PUT")
   $ItemDetailId = $_PUT['ItemDetailId'];
 
   // $qty = $ItemQty;
-  // $sql_qty_chk = "SELECT IF(Quantity <   $qty, $qty = Quantity +   $qty , $qty = $ItemQty ) From ProductStock 
+  // $sql_qty_chk = "SELECT IF(Quantity <> $qty, $qty = Quantity + $qty , $qty = $ItemQty ) From ProductStock 
   // WHERE itemdetailId = '$ItemDetailId' AND companyId = $companyId;";
   // mysqli_query($con,$sql_qty_chk);
 
-$sql_update_item = "UPDATE ProductStock SET 
- Quantity = Quantity + $ItemQty,TotalQty =  $totalQty,
- ReorderLabel =   $ItemReorderLabel 
- WHERE itemdetailId = '$ItemDetailId' AND companyId = $companyId";
+$sql = "UPDATE ProductStock
+SET ReorderLabel = $ItemReorderLabel 
+WHERE itemdetailId = '$ItemDetailId' AND companyId = $companyId";
+
+if(mysqli_query($con,$sql) or die(mysqli_error($con))){
+  $response['msg'] = 'Inventory '.$ItemName.' Information Updated Successfully';
+}else {
+  $response['msg'] = 'Server Error Please Try Again';
+  }
 
 $sql_update_item = "UPDATE ProductStock SET 
- Quantity = $qty,TotalQty =  $totalQty,
- ReorderLabel =   $ItemReorderLabel 
- WHERE itemdetailId = $ItemDetailId AND companyId = $companyId";
+ Quantity = Quantity + $ItemQty,TotalQty =  $totalQty
+ WHERE Quantity <> $ItemQty AND itemdetailId = '$ItemDetailId' AND companyId = $companyId ";
 
 if(mysqli_query($con,$sql_update_item) or die(mysqli_error($con))){
     $response['msg'] = 'Inventory '.$ItemName.' Information Updated Successfully';
 }else {
 $response['msg'] = 'Server Error Please Try Again';
 }
+
 }
 
 mysqli_close($con);
