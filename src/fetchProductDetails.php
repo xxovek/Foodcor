41ad58885442e3a90f38a6950b7e7098e6570d2f
vbpawar <vikaspawar3110@gmail.com>
2019-04-07 18:17:12 +0530

    changes in code

--- src/fetchProductDetails.php
+++ src/fetchProductDetails.php
@@ -6,7 +6,7 @@ if(isset($_SESSION['company_id'])){
 }else{
   $companyId = $_POST['company_id'];
 }
-$sql = "SELECT IM.Description,IM.ItemId,IM.ItemName,IM.SKU,IM.HSN,IM.Unit,SM.SizeValue,PS.Quantity,PS.TotalQty,PS.ReorderLabel,IP.price,ID.SubPacking
+$sql = "SELECT IM.Description,IM.ItemId,IM.ItemName,IM.SKU,IM.HSN,IM.Unit,SM.SizeValue,PS.Quantity,PS.TotalQty,PS.ReorderLabel,IP.price,ID.SubPacking,ID.PackingQty
 FROM ItemMaster IM
 LEFT JOIN ItemDetailMaster ID ON IM.ItemId = ID.ItemId LEFT JOIN ItemPrice IP ON IP.ItemDetailId = ID.itemDetailId
 LEFT JOIN ProductStock PS ON PS.itemdetailId = ID.itemDetailId
@@ -18,7 +18,7 @@ if($result = mysqli_query($con,$sql)){
     while($row = mysqli_fetch_array($result)){
       array_push($response,[
         'ItemId' => $row['ItemId'],
-        'ItemName' => ucfirst($row['ItemName'].' '.$row['SizeValue'].' '.$row['Unit']),
+        'ItemName' => ucfirst($row['ItemName'].' '.$row['SizeValue'].' '.$row['Unit'].' * '.$row['PackingQty'].'Pieces'),
         'SKU' => $row['SKU'],
         'HSN' => $row['HSN'],
         'SubPacking' => $row['SubPacking'],
