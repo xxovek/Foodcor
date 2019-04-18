4e8861a945c63575f8d719cf32051fa49a6e6e68
krkunal29 <krkunal29@gmail.com>
2019-04-18 14:02:49 +0530

    # kunal change db name

--- config/connection.php
+++ config/connection.php
@@ -2,7 +2,7 @@
 $servername = 'localhost';
 $username   = 'root';
 $password   = '';
-$dbname = 'fastinvofoodcor';
+$dbname = 'fastinvodemo';
 // $dbname     = 'Foodcor1';
 $con = new mysqli($servername,$username,$password,$dbname) or die(mysqli_error($con));
 ?>
